8892c239e0632cb23b3fbeecbb347bbd6018bb55
KaiHaverz <837597708@qq.com>
2024-08-29 22:22:23 +0800

    更新了 lec01

--- lec01-intro/ex3-归并排序.py
+++ lec01-intro/ex3-归并排序.py
@@ -18,4 +18,5 @@ def merge_sort(data_list):
 data_list = [random.randint(1, 100) for _ in range(50)]
 print(merge_sort(data_list))
 
-
+data_list2 = [16,4,23,22,11,2,8,39,10,7,21]
+print(merge_sort(data_list2))
