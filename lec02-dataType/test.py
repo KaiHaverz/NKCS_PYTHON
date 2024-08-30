697ef72186f613bdccbca78319b427c9c59f49bc
KaiHaverz <837597708@qq.com>
2024-08-30 20:38:58 +0800

    Day 3 learning record

---
+++ lec02-dataType/test.py
@@ -0,0 +1,11 @@
+print(12+999+0.008234)
+print("-------------")
+
+print("Ganchao39 "*3)
+print("-------------")
+
+import math
+print(math.sqrt(12))
+print("-------------")
+
+print("2**100=",2**100)
