3a61d4c94321e697439b25670ad681fd2a2b2f33
KaiHaverz <837597708@qq.com>
2024-08-31 13:02:35 +0800

    update Aug 31 learning

--- lec02-dataType/小计算.py
+++ lec02-dataType/小计算.py
@@ -1,12 +1,13 @@
 print(12+999+0.008234)
 print("-------------")
 
-print(("Ganchao"+"39 ")*99)
+print(("Ganchao"+"39 ")*5)
 print("-------------")
 
 import math
 # 求平方根
-print(math.sqrt(12))
+print("sqrt(12)=",math.sqrt(12))
+print("sqrt(2)=",math.sqrt(2))
 print("-------------")
 
-print("2**1000=",2**1000)
+print("2**10=",2**10)
